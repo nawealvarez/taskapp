49970490738bc92b4d1eadbcbcd51cca99318088
Nahuel Alvarez <nawe7depb@gmail.com>
2025-05-08 01:22:57 -0300

    feat: add axios config

---
+++ src/lib/axios.ts
@@ -0,0 +1,24 @@
+import axios, { type AxiosResponse } from 'axios';
+
+const apiClient = axios.create({
+  baseURL:
+    import.meta.env.VITE_API_URL || 'https://autodesk.free.beeceptor.com',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const apiRequest = async <T>(
+  url: string,
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+  data?: unknown
+): Promise<T> => {
+  const response: AxiosResponse<T> = await apiClient({
+    method,
+    url,
+    data,
+  });
+
+  return response.data;
+};
