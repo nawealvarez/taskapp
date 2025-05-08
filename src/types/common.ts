ade18627aebb4936f94e8bd7ad4c8dec256848da
Nahuel Alvarez <nawe7depb@gmail.com>
2025-05-08 01:22:35 -0300

    feat: add types

---
+++ src/types/common.ts
@@ -0,0 +1,18 @@
+export enum Status {
+  OPEN = 'Open',
+  IN_PROGRESS = 'In Progress',
+  CLOSED = 'Closed',
+  CANCELLED = 'Cancelled',
+}
+
+export enum Response {
+  APPROVE = 'Approve',
+  REJECT = 'Reject',
+  PENDING = 'Pending',
+}
+
+export enum Priority {
+  NORMAL = 'Normal',
+  HIGH = 'High',
+  LOW = 'Low',
+}
---
+++ src/types/item.ts
@@ -0,0 +1,15 @@
+import type { Status, Response, Priority } from "./common";
+
+export interface Item {
+  status: Status;
+  number: number;
+  spec: string;
+  rev: number;
+  title: string;
+  type: string;
+  priority: Priority;
+  package: string;
+  ballInCourt: string;
+  dueDate: string;
+  response: Response;
+}
